074cb24b9feb11582ed0ba07965a9be1b4c6613a
Joshua "Mart" Miller <32748201+Merchant151@users.noreply.github.com>
2019-10-18 23:45:56 -0400

    Added base and edited code

---
+++ 2DAnimAndCanvas/Assets/JW_Stuff/Script/JW_PLayerController.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JW_PLayerController : MonoBehaviour {
+    float speed = 5;
+    //My Components
+    Rigidbody2D myBod;
+    SpriteRenderer mySprite;
+    Animator myAnim;
+	// Use this for initialization
+	void Start () {
+        myBod = GetComponent<Rigidbody2D>();
+        mySprite = GetComponent<SpriteRenderer>();
+        myAnim = GetComponent<Animator>();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float h = Input.GetAxis("Horizontal");
+        myBod.velocity = new Vector2(h * speed, myBod.velocity.y);
+        myAnim.speed = speed * 0.3f;
+
+        if (h > 0)
+        {
+            mySprite.flipX = false;
+            myAnim.SetBool("IsRunning", true);
+        }
+        else if (h < 0)
+        {
+            mySprite.flipX = true;
+            myAnim.SetBool("IsRunning", true);
+        }
+        else
+        {
+            myAnim.SetBool("IsRunning", false);
+        }
+	}
+    public void changeSpeed (float f)
+    {
+        speed += f;
+    }
+}
---
+++ 2DAnimAndCanvas/Assets/JW_Stuff/Script/PotCounter.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PotCounter : MonoBehaviour {
+    public int startCount;
+    int count;
+    public KeyCode key;
+
+    //My Components
+    Text myText;
+    Button myButton;
+
+	// Use this for initialization
+	void Start () {
+        myText = GetComponentInChildren<Text>();
+        myButton = GetComponent<Button>();
+        setCount(startCount);
+        //key = KeyCode.Alpha0;
+
+	}
+	
+    void setCount (int c)
+    {
+        count = c;
+        myText.text = "" + count;
+        if(count <= 0)
+        {
+            myButton.interactable = false;
+        }
+    }
+
+    public void consume()
+    {
+        setCount(count - 1);
+    }
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown(key)&&count>=1)
+        {
+            consume();
+        }
+	}
+}
